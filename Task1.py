fee7dd14e7a790fefe88e9b6119a3b328537211a
Prachi Kamble <146330849+prachikamble914@users.noreply.github.com>
2024-03-12 01:04:52 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,9 @@
+print("********String Reversal********")
+def reverse_string(string1):
+    revstr=string1[::-1]
+
+    return revstr
+string1=input("enter a string:")
+r=reverse_string(string1)
+print(r)
+    
